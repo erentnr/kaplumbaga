c5581297abbd7705e78e63add8a19fd9475dcb33
eren <erentnr@yandex.com>
2022-01-03 04:44:57 +0300

    Add game handlers

--- server/game.py
+++ server/game.py
@@ -5,9 +5,24 @@ app = Flask(__name__)
 app.config['SECRET_KEY'] = 'temp_secret!'
 socketio = SocketIO(app, cors_allowed_origins='*')
 
+SAMPLE_TEXT = ("Artık bir satranç oynuyorduk, satrançta da en işe yaramaz "
+                "hamle feda etmek zorunda olduğun taşları okşamaktır.")
+
 @socketio.on('connect')
 def test_connect():
     print('Client connected')
+
+@socketio.on('game_start')
+def handle_game_start():
+    emit('load_text', SAMPLE_TEXT)
+
+@socketio.on('progress')
+def handle_progress(data):
+    emit('progress_percentage', data)
+
+@socketio.on('finish')
+def handle_progress():
+    emit('finish_game')
 
 if __name__ == '__main__':
     socketio.run(app)
